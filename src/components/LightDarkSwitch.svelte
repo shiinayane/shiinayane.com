9a9dc3e5fd6bd574e0fd412ea155298c397824d9
shiinayane <shiinayane@gmail.com>
2026-05-03 23:19:01 +0900

    fix: correct some errors

--- src/components/LightDarkSwitch.svelte
+++ src/components/LightDarkSwitch.svelte
@@ -11,6 +11,12 @@ import {
 import { onMount } from "svelte";
 import type { LIGHT_DARK_MODE } from "@/types/config.ts";
 
+type AstroClientDirectiveProps = {
+	"client:only"?: string;
+};
+
+let {}: AstroClientDirectiveProps = $props();
+
 const seq: LIGHT_DARK_MODE[] = [LIGHT_MODE, DARK_MODE, AUTO_MODE];
 let mode: LIGHT_DARK_MODE = $state(AUTO_MODE);
 
